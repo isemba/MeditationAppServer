dc5cd3c1ea0093f573d73679e1161a1ab976d6e0
Lotus-108 <hnurel@gmail.com>
2020-10-22 23:25:27 +0300

    db environment added

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const env = require('dotenv');
+
+env.config({
+    path: __dirname + "/.env"
+})
+
 
 const app = express();
 
@@ -16,8 +22,13 @@ app.get("/hello/:name", (req, res)=>{
 });
 
 
+const dbUser = process.env.DDB_USER;
+const dbPass = process.env.DDB_PASS;
 
-
+if(!dbUser || !dbPass){
+    console.error("DocumentDB DDB_USER OR DDB_PASS NOT FOUND!");
+    process.exit(1);
+}
 
 app.listen(3000, ()=>{
     console.log("server has started listening on port 3000");
